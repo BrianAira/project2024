cc1a039900350f0445b1ab6471147461ae92e67b
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2024-10-08 23:25:59 -0300

    se cambio el tipo de widget de articuloCard a statfullWidget  se implemento un hover sobre el icono de compra

--- lib/widgets/articuloCard.dart
+++ lib/widgets/articuloCard.dart
@@ -1,66 +1,117 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+//import 'package:flutter/rendering.dart';
 import '../DB/articulos.dart';
 import '../main.dart';
 import 'package:provider/provider.dart';
 
-class ArticuloCard extends StatelessWidget {
+class ArticuloCard extends StatefulWidget {
   final Articulo articulo;
 
   ArticuloCard({required this.articulo});
   @override
+  _ArticuloCardState createState() => _ArticuloCardState();
+}
+
+class _ArticuloCardState extends State<ArticuloCard> {
+  bool _isHover = false;
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 8,
-      shadowColor: Colors.grey.withOpacity(0.5),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        children: [
-          Container(
-            //borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-            constraints: BoxConstraints(maxHeight: 350),
-            child: Image.network(
-              articulo.imageUrl,
-              fit: BoxFit.cover,
-            ),
+    return Container(
+        width: 500,
+        height: 900,
+        child: Card(
+          elevation: 8,
+          shadowColor: Colors.grey.withOpacity(0.5),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  articulo.nombre,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                Text('Precio \$${articulo.precio.toStringAsFixed(2)}'),
-                Text('Cantidad: ${articulo.cantidad}'),
-                Text(articulo.descripcion),
-              ],
-            ),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              Consumer<MyAppState>(builder: (context, myAppState, child) {
-                final isFavorite = myAppState.favoritos.contains(articulo);
-                return TextButton(
-                    onPressed: () {
-                      myAppState.toggleFavorite(articulo);
-                    },
-                    child: Icon(isFavorite
-                        ? (Icons.favorite)
-                        : (Icons.favorite_border_outlined)));
-                //isFavorite ? "Eliminar de Favoritos" : "Favoritos"));
-              })
+              Container(
+                constraints: BoxConstraints(maxHeight: 650, minHeight: 250),
+                child: Image.network(
+                  widget.articulo.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.articulo.nombre,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                        'Precio \$${widget.articulo.precio.toStringAsFixed(2)}'),
+                    Text('Cantidad: ${widget.articulo.cantidad}'),
+                    Text(widget.articulo.descripcion),
+                  ],
+                ),
+              ),
+              Spacer(),
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Consumer<MyAppState>(builder: (context, myAppState, child) {
+                    final isFavorite =
+                        myAppState.favoritos.contains(widget.articulo);
+                    return ElevatedButton(
+                        onPressed: () {
+                          myAppState.toggleFavorite(widget.articulo);
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent, elevation: 0),
+                        child: Icon(
+                          isFavorite
+                              ? (Icons.favorite)
+                              : (Icons.favorite_border_outlined),
+                          color: isFavorite
+                              ? const Color.fromARGB(255, 230, 47, 34)
+                              : Colors.black,
+                        ));
+                  })
+                ],
+              ),
+              MouseRegion(
+                onEnter: (_) => setState(() {
+                  _isHover = true;
+                }),
+                onExit: (_) => setState(() {
+                  _isHover = false;
+                }),
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 200),
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                  decoration: BoxDecoration(
+                    color: _isHover
+                        ? Color.fromARGB(255, 255, 190, 27)
+                        : Color.fromARGB(255, 16, 68, 100),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          elevation: 0,
+                          shadowColor: Colors.transparent,
+                          padding: EdgeInsets.zero),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.add_shopping_cart_outlined,
+                            color: Colors.white,
+                            size: _isHover ? 28 : 24,
+                          ),
+                        ],
+                      )),
+                ),
+              )
             ],
           ),
-          TextButton(
-              onPressed: () {}, child: Icon(Icons.add_shopping_cart_outlined))
-        ],
-      ),
-    );
+        ));
   }
 }
