2b8b051400021ce80566b781720f470173c020c9
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2024-10-09 14:48:21 -0300

    se cambio el orden de los widgets en pantalla

--- lib/pages/articlesPage.dart
+++ lib/pages/articlesPage.dart
@@ -63,13 +63,20 @@ class _ArticlePageState extends State<ArticlePage> {
           builder: (context, constraints) {
             final double itemWith = 200;
 
-            int crossAxisCount = (constraints.maxWidth / itemWith).floor();
+            int crossAxisCount;
+            if (constraints.maxWidth >= itemWith * 3) {
+              crossAxisCount = 3;
+            } else if (constraints.maxWidth >= 500) {
+              crossAxisCount = 2;
+            } else {
+              crossAxisCount = 1;
+            }
             return GridView.builder(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: crossAxisCount,
                   crossAxisSpacing: 10,
                   mainAxisSpacing: 10,
-                  childAspectRatio: 3 / 4),
+                  childAspectRatio: 3 / 5),
               itemCount: _filteredArticles.length,
               itemBuilder: (context, index) {
                 return ArticuloCard(articulo: _filteredArticles[index]);
@@ -79,73 +86,3 @@ class _ArticlePageState extends State<ArticlePage> {
         ));
   }
 }
-
-//barra de busqueda --->
-//TextEditingController _searchController = TextEditingController();
-//List<Articulo> _filteredArticles = [];
-
-// Expanded(
-              //   child: Padding(
-              //     padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              //     child: TextField(
-              //       controller: _searchController,
-              //       decoration: InputDecoration(
-              //         hintText: 'Buscar producto...',
-              //         prefixIcon: Icon(Icons.search),
-              //         fillColor: Colors.white,
-              //         filled: true,
-              //         border: OutlineInputBorder(
-              //           borderRadius: BorderRadius.circular(30),
-              //           borderSide: BorderSide.none,
-              //         ),
-              //       ),
-              //     ),
-              //   ),
-              // ),
-
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _filteredArticles = articulos;
-  //   _searchController.addListener(_onSearchChanged);
-  // }
-
-  // void _onSearchChanged() {
-  //   setState(() {
-  //     _filteredArticles = articulos.where((articulo) {
-  //       return articulo.nombre
-  //               .toLowerCase()
-  //               .contains(_searchController.text.toLowerCase()) ||
-  //           articulo.tipo
-  //               .toLowerCase()
-  //               .contains(_searchController.text.toLowerCase());
-  //     }).toList();
-  //   });
-  // }
-
-  // @override
-  // void dispose() {
-  //   _searchController.dispose();
-  //   super.dispose();
-  // }
-
-              //   child: LayoutBuilder(
-              //     builder: (context, constraints) {
-              //       final double itemWidth = 200;
-              //       int crossAxisCount =
-              //           (constraints.maxWidth / itemWidth).floor();
-              //       return GridView.builder(
-              //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              //           crossAxisCount: crossAxisCount,
-              //           crossAxisSpacing: 10,
-              //           mainAxisSpacing: 10,
-              //           childAspectRatio: 3 / 4,
-              //         ),
-              //         itemCount: _filteredArticles.length,
-              //         itemBuilder: (context, index) {
-              //           return ArticuloCard(articulo: _filteredArticles[index]);
-              //         },
-              //       );
-              //     },
-              //   ),
