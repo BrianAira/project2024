41ebf835b96fa47b0754aa4172db5764dc5b64e1
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2024-11-26 18:03:17 -0300

    modificacion en los widgets de campos de textos. se corrigio el error que imposibilitaba el redireccionamiento a la pagina principal

--- lib/login.dart
+++ lib/login.dart
@@ -1,219 +1,260 @@
 import 'package:flutter/material.dart';
-//import 'package:myapp/main.dart';
+import 'package:flutter/rendering.dart';
+import './main.dart';
+//import 'package:flutter/widgets.dart';
 //import 'package:myapp/main.dart';
 //import 'package:shared_preferences/shared_preferences.dart';
+import './DB/usuarios.dart';
 
-// class login extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Login',
-//       home: SplashScreen(),
-//     );
-//   }
-// }
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-// class SplashScreen extends StatefulWidget {
-//   @override
-//   _SplashScreenState createState() => _SplashScreenState();
-// }
+class _LoginPageState extends State<LoginPage> {
+  final _usuarioController = TextEditingController();
+  //final _emailController = TextEditingController();
+  final _contrasenaController = TextEditingController();
+  String text = 'Ingrese los datos por favor';
 
-// class _SplashScreenState extends State<SplashScreen> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     _checkLoginStatus();
-//   }
+  void _login() {
+    String nombreUsuario = _usuarioController.text.trim();
+    String contrasenaUsuario = _contrasenaController.text.trim();
 
-//   void _checkLoginStatus() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     bool isLoggedIn = prefs.getBool('isLoggedin') ?? false;
+    if (nombreUsuario.isEmpty || contrasenaUsuario.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Por favor completa los campos')));
+      return;
+    }
 
-//     if (isLoggedIn) {
-//       Navigator.pushReplacement(
-//         context,
-//         MaterialPageRoute(builder: (context) => LoginPage()),
-//       );
-//     } else {
-//       Navigator.pushReplacement(
-//         context,
-//         MaterialPageRoute(builder: (context) => LoginPage()),
-//       );
-//     }
-//   }
+    final usuario = usuarios.firstWhere(
+      (element) =>
+          element?.nombre == nombreUsuario &&
+          element?.contrasena == contrasenaUsuario,
+      orElse: () => null,
+    );
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: CircularProgressIndicator(),
-//       ),
-//     );
-//   }
-// }
+    if (usuario == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Usuario o contraseña incorrectos')),
+      );
+      // Detener ejecución si no se encuentra el usuario
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MyHomePage(
+            title: 'Flutter Demo Home Page',
+          ),
+        ),
+      );
+    }
+  }
 
-//Login page
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Container(
+        //implementeacion de la imagen de fondo
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/fondo.jpeg'), // Usa la imagen local
+            fit: BoxFit.cover, // La imagen rellena toda la pantalla
+          ),
+        ),
+        child: Center(
+          child: Column(
+            //cuadros de inicio de sesion
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Inicia Sesión",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 255, 251, 251), fontSize: 28.0),
+              ),
 
-// class LoginPage extends StatefulWidget {
-//   @override
-//   _LoginPageState createState() => _LoginPageState();
-// }
+              const SizedBox(height: 20),
+              // Espacio entre el texto y el campo de usuario
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  // Calculamos el ancho dinámicamente
+                  double width = constraints.maxWidth *
+                      0.8; // Usa el 80% del ancho disponible
 
-// class _LoginPageState extends State<LoginPage> {
-//   final _emailController = TextEditingController();
-//   final _passwordController = TextEditingController();
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8), // Ajusta el padding vertical
+                    child: Center(
+                      child: SizedBox(
+                        width: width > 400
+                            ? 400
+                            : width, // Máximo ancho 400, ajusta según la pantalla
+                        child: TextField(
+                          controller:
+                              _usuarioController, // Conecta el controlador
+                          style: const TextStyle(color: Colors.white),
+                          decoration: const InputDecoration(
+                            hintText: "User",
+                            hintStyle: TextStyle(color: Colors.white),
+                            border: OutlineInputBorder(), // Borde estándar
+                            filled: true,
+                            fillColor: Color(
+                                0xFF2C2C34), // Fondo oscuro para contraste
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ), // Aquí se muestra el campo de usuario
+              LayoutBuilder(builder: (context, constraints) {
+                double width = constraints.maxWidth * 0.8;
+                return Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: 8), // Añadimos un poco de padding horizontal
+                    child: Center(
+                        child: SizedBox(
+                      width: width > 400 ? 400 : width,
+                      child: TextField(
+                        controller: _contrasenaController,
+                        style: TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.white),
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Color(
+                                0xFF2C2C34) // Añadir un borde al campo de texto
+                            ),
+                      ),
+                    )));
+              }),
+              const SizedBox(height: 20), // Espacio entre el campo y el botón
+              ElevatedButton(
+                onPressed: _login,
+                child: Text("Ingresar"),
+              ),
+              nuevaCuenta(), // Aquí se agrega el botón debajo del campo de usuario
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-//   void _login() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-
-//     if (_emailController.text == 'user@example.com' &&
-//         _passwordController.text == 'password') {
-//       prefs.setBool('isLoggedIn', true);
-//       Navigator.pushReplacement(
-//         context,
-//         MaterialPageRoute(builder: (context) => MyApp()),
-//       );
-//     } else {
-//       ScaffoldMessenger.of(context)
-//           .showSnackBar(SnackBar(content: Text('credencial invalida')));
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Login'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             TextField(
-//               controller: _emailController,
-//               decoration: InputDecoration(labelText: 'Email'),
-//             ),
-//             TextField(
-//               controller: _passwordController,
-//               decoration: InputDecoration(labelText: 'Password'),
-//               obscureText: true,
-//             ),
-//             SizedBox(
-//               height: 20,
-//             ),
-//             ElevatedButton(
-//               onPressed: _login,
-//               child: Text('Iniciar Sesion'),
-//             ),
-//             TextButton(
-//               onPressed: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(builder: (context) => RegistrationPage()),
-//                 );
-//               },
-//               child: Text('Crear una cuenta nueva'),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class RegistrationPage extends StatefulWidget {
-//   @override
-//   _RegistrationPageState createState() => _RegistrationPageState();
-// }
-
-// class _RegistrationPageState extends State<RegistrationPage> {
-//   final _emailController = TextEditingController();
-//   final _passwordController = TextEditingController();
-
-//   void _register() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     prefs.setBool('isLoggedIn', true)
-//   }
-// }
-// Widget build(BuildContext context) {
-//   return Scaffold(
-//     appBar: AppBar(
-//       title: Text("Login"),
-//     ),
-//     body: Center(
-//       child: Text("Hola"),
-//     ),
-//   );
-// }
-
-// Widget cuerpo() {
+// Widget imagen() {
 //   return Container(
-//     decoration: BoxDecoration(
+//     decoration: const BoxDecoration(
 //       image: DecorationImage(
-//           image: NetworkImage(
-//               "https://img.freepik.com/vector-gratis/fondo-futurista-degradado-geometrico_23-2149116406.jpg"),
-//           fit: BoxFit.cover),
+//         image: AssetImage('assets/fondo.jpeg'), // Usa la imagen local
+//         fit: BoxFit.cover, // La imagen rellena toda la pantalla
+//       ),
 //     ),
 //     child: Center(
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: <Widget>[
-//           nombre(),
-//           campoUsuario(),
-//           campoContrasena(),
-//           SizedBox(
-//             height: 10,
-//           ),
-//           botonEntrar(),
-//         ],
-//       ),
+//       child: iniciarSesion(),
 //     ),
 //   );
 // }
 
-// Widget nombre() {
+// Widget iniciarSesion() {
+//   return Column(
+//     mainAxisAlignment: MainAxisAlignment.center,
+//     children: [
+//       const Text(
+//         "Inicia Sesión",
+//         style: TextStyle(
+//             color: Color.fromARGB(255, 255, 251, 251), fontSize: 28.0),
+//       ),
+
+//       const SizedBox(height: 20),
+//       // Espacio entre el texto y el campo de usuario
+//       campoUsuario(_LoginPageState()
+//           ._usuarioController), // Aquí se muestra el campo de usuario
+//       campoContrasena(_LoginPageState()._contrasenaController),
+//       const SizedBox(height: 20), // Espacio entre el campo y el botón
+//       ingresar(),
+//       nuevaCuenta(), // Aquí se agrega el botón debajo del campo de usuario
+//     ],
+//   );
+// }
+
+// Widget mensaje() {
 //   return Text(
-//     "Sign in ",
-//     style: TextStyle(
-//         color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold),
+//     'Por favor ingrese los datos',
+//     style: TextStyle(color: Color.fromARGB(255, 255, 255, 255), fontSize: 18),
 //   );
 // }
 
-// Widget campoUsuario() {
-//   return Container(
-//     padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-//     child: TextField(
-//       decoration: InputDecoration(
-//         hintText: "User",
-//         fillColor: Colors.white,
-//         filled: true,
-//       ),
-//     ),
+// Widget campoUsuario(TextEditingController controller) {
+//   return LayoutBuilder(
+//     builder: (context, constraints) {
+//       // Calculamos el ancho dinámicamente
+//       double width =
+//           constraints.maxWidth * 0.8; // Usa el 80% del ancho disponible
+
+//       return Padding(
+//         padding: const EdgeInsets.symmetric(
+//             vertical: 8), // Ajusta el padding vertical
+//         child: Center(
+//           child: SizedBox(
+//             width: width > 400
+//                 ? 400
+//                 : width, // Máximo ancho 400, ajusta según la pantalla
+//             child: TextField(
+//               controller: controller, // Conecta el controlador
+//               style: const TextStyle(color: Colors.white),
+//               decoration: const InputDecoration(
+//                 hintText: "User",
+//                 hintStyle: TextStyle(color: Colors.white),
+//                 border: OutlineInputBorder(), // Borde estándar
+//                 filled: true,
+//                 fillColor: Color(0xFF2C2C34), // Fondo oscuro para contraste
+//               ),
+//             ),
+//           ),
+//         ),
+//       );
+//     },
 //   );
 // }
 
-// Widget campoContrasena() {
-//   return Container(
-//     padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-//     child: TextField(
-//       obscureText: true,
-//       decoration: InputDecoration(
-//         hintText: "User",
-//         fillColor: Colors.white,
-//         filled: true,
-//       ),
-//     ),
+// Widget campoContrasena(TextEditingController controller) {
+//   return LayoutBuilder(builder: (context, constraints) {
+//     double width = constraints.maxWidth * 0.8;
+//     return Padding(
+//         padding: EdgeInsets.symmetric(
+//             vertical: 8), // Añadimos un poco de padding horizontal
+//         child: Center(
+//             child: SizedBox(
+//           width: width > 400 ? 400 : width,
+//           child: TextField(
+//             controller: controller,
+//             style: TextStyle(color: Colors.white),
+//             decoration: const InputDecoration(
+//                 hintText: "Password",
+//                 hintStyle: TextStyle(color: Colors.white),
+//                 border: OutlineInputBorder(),
+//                 filled: true,
+//                 fillColor:
+//                     Color(0xFF2C2C34) // Añadir un borde al campo de texto
+//                 ),
+//           ),
+//         )));
+//   });
+// }
+
+// Widget ingresar() {
+//   return ElevatedButton(
+//     onPressed: _LoginPageState()._login,
+//     child: Text("Ingresar"),
 //   );
 // }
 
-Widget botonEntrar() {
+Widget nuevaCuenta() {
   return ElevatedButton(
-    onPressed: () {},
-    child: Text("Enter"),
-    style: ElevatedButton.styleFrom(
-      padding: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
-      textStyle: TextStyle(fontSize: 20, color: Colors.white),
-    ),
-  );
+      onPressed: () {}, child: const Text('Crear nueva cuenta'));
 }
